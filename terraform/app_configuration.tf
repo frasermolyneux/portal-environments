1333676071f4479d5347de79ce22942bf7424f03
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-10-03 21:14:40 +0100

    Add role assignment for App Configuration Data Owner

--- terraform/app_configuration.tf
+++ terraform/app_configuration.tf
@@ -13,3 +13,10 @@ resource "azurerm_app_configuration" "app_configuration" {
 
   tags = var.tags
 }
+
+resource "azurerm_role_assignment" "deploy_sp_app_config_data_owner" {
+  scope                = azurerm_app_configuration.app_configuration.id
+  role_definition_name = "App Configuration Data Owner"
+  principal_id         = data.azurerm_client_config.current.object_id
+  principal_type       = "ServicePrincipal"
+}
