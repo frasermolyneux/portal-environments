be3778c4e33833f955a1cf055220907c741d57cb
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-10-03 22:38:10 +0100

    Add outputs for user-assigned identities and their principal IDs in Terraform configuration

---
+++ terraform/outputs.tf
@@ -0,0 +1,55 @@
+output "api_management_identity_id" {
+  value = azurerm_user_assigned_identity.api_management_identity.id
+}
+
+output "public_webapp_identity_id" {
+  value = azurerm_user_assigned_identity.public_webapp_identity.id
+}
+
+output "repository_webapi_identity_id" {
+  value = azurerm_user_assigned_identity.repository_webapi_identity.id
+}
+
+output "message_broker_funcapp_identity_id" {
+  value = azurerm_user_assigned_identity.message_broker_funcapp_identity.id
+}
+
+output "event_ingest_funcapp_identity_id" {
+  value = azurerm_user_assigned_identity.event_ingest_funcapp_identity.id
+}
+
+output "sync_funcapp_identity_id" {
+  value = azurerm_user_assigned_identity.sync_funcapp_identity.id
+}
+
+output "repository_funcapp_identity_id" {
+  value = azurerm_user_assigned_identity.repository_funcapp_identity.id
+}
+
+output "api_management_identity_principal_id" {
+  value = azurerm_user_assigned_identity.api_management_identity.principal_id
+}
+
+output "public_webapp_identity_principal_id" {
+  value = azurerm_user_assigned_identity.public_webapp_identity.principal_id
+}
+
+output "repository_webapi_identity_principal_id" {
+  value = azurerm_user_assigned_identity.repository_webapi_identity.principal_id
+}
+
+output "message_broker_funcapp_identity_principal_id" {
+  value = azurerm_user_assigned_identity.message_broker_funcapp_identity.principal_id
+}
+
+output "event_ingest_funcapp_identity_principal_id" {
+  value = azurerm_user_assigned_identity.event_ingest_funcapp_identity.principal_id
+}
+
+output "sync_funcapp_identity_principal_id" {
+  value = azurerm_user_assigned_identity.sync_funcapp_identity.principal_id
+}
+
+output "repository_funcapp_identity_principal_id" {
+  value = azurerm_user_assigned_identity.repository_funcapp_identity.principal_id
+}
