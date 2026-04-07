bd3c05237a761a6fa55f6fb9da894d236fd6fbaf
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-04-07 17:55:30 +0100

    feat: add custom subdomain name to content safety cognitive account

--- terraform/content_safety.tf
+++ terraform/content_safety.tf
@@ -1,9 +1,10 @@
 resource "azurerm_cognitive_account" "content_safety" {
-  name                = "cs-portal-${var.environment}-${var.location}"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  kind                = "ContentSafety"
-  sku_name            = "S0"
+  name                  = "cs-portal-${var.environment}-${var.location}"
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  kind                  = "ContentSafety"
+  sku_name              = "S0"
+  custom_subdomain_name = "cs-portal-${var.environment}-${var.location}"
 
   identity {
     type = "SystemAssigned"
